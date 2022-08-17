4d30f9697f58699576eff4acb43ed1b010b8451d
Jens Brejner <jens.brejner@eficode.com>
2022-08-17 12:33:40 +0000

    cCheck if docker actually is installed

--- ci/performance-test.sh
+++ ci/performance-test.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
+
+echo bash: $(which bash)
+echo docker: $(which docker)
+echo docker-compose: $(which docker-compose)
+
+
 [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
 docker_username=$DockerRepo docker-compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
