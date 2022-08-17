a0f09ab672927453d677f61b25cdc226018777a0
Jens Brejner <jens.brejner@eficode.com>
2022-08-17 12:48:40 +0000

    Make the test in the correct file

--- ci/component-test.sh
+++ ci/component-test.sh
@@ -1,3 +1,9 @@
 #! /bin/bash
-[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-docker_username=$DockerRepo docker-compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
+
+
+echo bash: $(which bash)
+echo docker: $(which docker)
+echo docker-compose: $(which docker-compose)
+
+# [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+# docker_username=$DockerRepo docker-compose -f component-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
--- ci/performance-test.sh
+++ ci/performance-test.sh
@@ -1,8 +1,3 @@
 #! /bin/bash
-
-echo bash: $(which bash)
-echo docker: $(which docker)
-echo docker-compose: $(which docker-compose)
-
-# [[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
-# docker_username=$DockerRepo docker-compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
+[[ -z "${docker_username}" ]] && DockerRepo='' || DockerRepo="${docker_username}/"
+docker_username=$DockerRepo docker-compose -f performance-test/docker-compose.yml --project-directory . -p ci up --build --exit-code-from test
